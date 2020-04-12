2af29a971f9c46b23ad7118e5d8d78adcb6fa486
Nozomi Okada <nokada@proofpoint.com>
2020-04-12 16:18:53 -0600

    Initial commit

---
+++ Yosomono/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Yosomono
+//
+//  Created by Nozomi Okada on 4/12/20.
+//  Copyright © 2020 nozokada. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
