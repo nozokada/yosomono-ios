f52f0e0ea70cf34aa93a10b6f0248395983b0423
Nozomi Okada <nozokada@gmail.com>
2020-09-18 23:58:45 -0600

    Changed folder structure

---
+++ Yosomono/Utility/SceneDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  SceneDelegate.swift
+//  Yosomono
+//
+//  Created by Nozomi Okada on 8/17/20.
+//  Copyright © 2020 Nozomi Okada. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        // Create the SwiftUI view that provides the window contents.
+        let contentView = AppRootView().environmentObject(AuthenticationService())
+
+        // Use a UIHostingController as window root view controller.
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+}
