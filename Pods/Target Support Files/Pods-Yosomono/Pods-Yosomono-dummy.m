7e0d7b9c9fbf0d98c962a46acde280670808f63d
Nozomi Okada <nozokada@gmail.com>
2020-09-19 00:02:06 -0600

    Ignored Pods files

--- Pods/Target Support Files/Pods-Yosomono/Pods-Yosomono-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Yosomono : NSObject
-@end
-@implementation PodsDummy_Pods_Yosomono
-@end
--- Pods/Target Support Files/Pods-YosomonoTests/Pods-YosomonoTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_YosomonoTests : NSObject
-@end
-@implementation PodsDummy_Pods_YosomonoTests
-@end
